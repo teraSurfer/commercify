019c0ba4364183cba2aaa63ab3e7a5ea9c8b2437
teraSurfer <achalaesh@live.com>
2021-07-23 21:51:16 -0700

    cors

--- src/main/java/com/commercity/monolith/CommercifyMonolithApplication.java
+++ src/main/java/com/commercity/monolith/CommercifyMonolithApplication.java
@@ -2,6 +2,9 @@ package com.commercity.monolith;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CommercifyMonolithApplication {
@@ -10,4 +13,13 @@ public class CommercifyMonolithApplication {
         SpringApplication.run(CommercifyMonolithApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/graphql").allowedOrigins("*");
+            }
+        };
+    }
 }
